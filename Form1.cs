11e3b412c25e5160530db286db8e9d40dc14e746
YoungSoo Choi <maninzoo@inswave.com>
2024-08-20 13:46:15 +0900

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using Utility;
+
+namespace WindowsFormsApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int pid = Process.GetCurrentProcess().Id;
+            MinidumpWriter.MakeDump(@"c:\temp\test.dmp", pid);
+        }
+    }
+}
